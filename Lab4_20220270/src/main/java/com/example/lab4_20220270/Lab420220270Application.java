8a7117716f9146f3d3ef5da228a9806647356960
Christian Flores <167637198+Cjfs2005@users.noreply.github.com>
2025-05-09 20:40:15 -0500

    Commit inicial

---
+++ Lab4_20220270/src/main/java/com/example/lab4_20220270/Lab420220270Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20220270;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420220270Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420220270Application.class, args);
+    }
+
+}
---
+++ Lab4_20220270/src/test/java/com/example/lab4_20220270/Lab420220270ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20220270;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420220270ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
